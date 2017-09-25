9041c33ed2275625ce543421470f80d54db26003
Vlad Zhukov <work4vlad@gmail.com>
2017-09-25 12:10:00 +0300

    Upgrade to fela 6

--- src/withStyle.js
+++ src/withStyle.js
@@ -1,13 +1,14 @@
 import {createElement} from 'react'; // eslint-disable-line
 import PropTypes from 'prop-types';
-import {createComponentFactory} from 'fela';
+import {createComponentFactory} from 'fela-bindings';
+import {withTheme} from 'react-fela';
 
 const propTypes = {
     renderer: PropTypes.object,
     theme: PropTypes.object,
 };
 
-const factory = createComponentFactory(createElement, propTypes);
+const factory = createComponentFactory(createElement, withTheme, propTypes);
 
 export function passThroughProps(props) {
     const keys = Object.keys(props);
